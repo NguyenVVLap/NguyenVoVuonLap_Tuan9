f360e1b494bb7bf75fc4a0e5b6d142ebc539ee27
NguyenVVLap <nlap741@gmail.com>
2022-11-06 16:07:57 +0700

    finis cau 2 cloud-config-server

--- code/DepartmentApp/src/main/java/com/example/DepartmentApp/rest/DepartmentRestController.java
+++ code/DepartmentApp/src/main/java/com/example/DepartmentApp/rest/DepartmentRestController.java
@@ -3,6 +3,8 @@ package com.example.DepartmentApp.rest;
 import com.example.DepartmentApp.entity.Department;
 import com.example.DepartmentApp.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RefreshScope
 public class DepartmentRestController {
     @Autowired
     private DepartmentService departmentService;
 
+    @Value("${welcome}")
+    private String welcome;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return welcome;
+    }
     @GetMapping("/departments")
     public List<Department> getAll() {
         return departmentService.getAll();
--- code/UserApp/src/main/java/com/example/UserApp/rest/UserRestController.java
+++ code/UserApp/src/main/java/com/example/UserApp/rest/UserRestController.java
@@ -3,6 +3,7 @@ package com.example.UserApp.rest;
 import com.example.UserApp.entity.User;
 import com.example.UserApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +16,20 @@ public class UserRestController {
     @Autowired
     private UserService userService;
 
+    @Value("${welcome}")
+    private String welcome;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return welcome;
+    }
+
     @GetMapping("/users")
     public List<User> getAll() {
         return userService.getAll();
     }
+
+
 
     @PostMapping("/users")
     public User save(@RequestBody User user) {
